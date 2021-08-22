f701e3cee4ea3b1d338115a5c1399622166d20e8
Rui Hirokawa <rui.hirokawa@gmail.com>
2021-08-22 23:13:40 +0900

    -- added data, license files.

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md","r") as f:
+    long_description = f.read()
+    
+setuptools.setup(
+    name="cssrlib",
+    version="0.0.1",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "scipy",
+        "cbitstruct",
+        "ephem",
+    ],
+    author='Rui Hirokawa',
+    author_email="",
+    description="Compact SSR Library for PPP/PPP-RTK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hirokawa/cssrlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
+
+
+
