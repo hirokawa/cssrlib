fd568cf7d226794323640b9efdfaa2717ee188af
Hauschild, André <andre.hauschild@dlr.de>
2023-03-23 16:26:24 +0100

    Major revision of ANTEX test

--- src/cssrlib/test/test_peph.py
+++ src/cssrlib/test/test_peph.py
@@ -3,31 +3,29 @@ Test script for peph module
 """
 import numpy as np
 from os.path import expanduser
-from cssrlib.peph import biasdec, peph, satantoff, readpcv
+from cssrlib.peph import biasdec, peph, satantoff, readpcv, searchpcv
 from cssrlib.rinex import rnxdec
-from cssrlib.gnss import Nav, epoch2time, time2epoch, timeadd, rSIG
+from cssrlib.gnss import Nav, epoch2time, time2epoch, timeadd, sat2id
 
 
 bdir = expanduser('~/GNSS_DAT/')
+atxfile = bdir+"IGS/ANTEX/igs14.atx"
 orbfile = bdir+"COD0IGSRAP/2021/COD0IGSRAP_20210780000_01D_15M_ORB.SP3"
 clkfile = bdir+"COD0IGSRAP/2021/COD0IGSRAP_20210780000_01D_30S_CLK.CLK"
-atxfile = bdir+"IGS/ANTEX/igs14.atx"
 dcbfile = bdir+"COD0IGSRAP/2021/COD0IGSRAP_20210780000_01D_01D_OSB.BIA"
-#dcbfile = bdir+"CAS0MGXRAP/2021/CAS0MGXRAP_20210780000_01D_01D_DCB.BSX"
-#dcbfile = bdir+"CAS0MGXRAP/2021/CAS0MGXRAP_20210780000_01D_01D_OSB.BIA"
 
-time = epoch2time([2021, 3, 19, 0, 0, 0])
-sat = 3
+time = epoch2time([2021, 3, 19, 12, 0, 0])
+sat = 1
 
 rnx = rnxdec()
 nav = Nav()
+sp = peph()
 
-if False:
+if True:
 
-    sp = peph()
     nav = sp.parse_sp3(orbfile, nav)
     nav = rnx.decode_clk(clkfile, nav)
-    nav.pcvs = readpcv(atxfile)
+    nav.pcvs, nav.pcvr = readpcv(atxfile)
 
     n = 10
     rs = np.zeros((1, 6))
@@ -37,17 +35,41 @@ if False:
         rs[0, :], dts[0, :], var = sp.peph2pos(t, sat, nav)
 
         ep = time2epoch(t)
-        print("{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}  {:02d}  "
+        print("{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}  {:s}  "
               "{:15.4f} {:15.4f} {:15.4f} {:15.6f}"
-              .format(ep[0], ep[1], ep[2], ep[3], ep[4], ep[5], sat,
+              .format(ep[0], ep[1], ep[2], ep[3], ep[4], ep[5], sat2id(sat),
                       rs[0, 0], rs[0, 1], rs[0, 2], dts[0, 0]*1e6))
 
-if False:
+if True:
+
+    nav = sp.parse_sp3(orbfile, nav)
+    nav.pcvs, nav.pcvr = readpcv(atxfile)
+
+    """
+    for pcv in nav.pcvs:
+        print(sat2id(pcv.sat), pcv.type)
+    """
+
+    pcv = searchpcv(sat, time, nav.pcvs)
+    print(pcv.sat, pcv.type)
+
+    name = "TRM57971.00     NONE"
+    pcv = searchpcv(name, time, nav.pcvr)
+    print(pcv.sat, pcv.type)
 
     rs, dts, var = sp.peph2pos(time, sat, nav)
     off = satantoff(time, rs[0:3], sat, nav)
+    print(off)
 
-if True:
+    """
+    ep = time2epoch(time)
+    print("{:4d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}  {:s}  "
+          "{:15.4f} {:15.4f} {:15.4f}"
+          .format(ep[0], ep[1], ep[2], ep[3], ep[4], ep[5], sat2id(sat),
+                  off[0], off[1], off[2]))
+    """
+
+if False:
 
     bd = biasdec()
     bd.parse(dcbfile)
@@ -56,10 +78,10 @@ if True:
     bias, std, = bd.getosb(sat, time, sig)
     assert bias == 7.6934
     assert std == 0.0
-    print("{:02d} {:s} {:8.5f} {:6.4f}".format(sat, sig, bias, std))
+    print("{:s} {:s} {:8.5f} {:6.4f}".format(sat2id(sat), sig, bias, std))
 
     sig = "L1W"
     bias, std, = bd.getosb(sat, time, sig)
     assert bias == 0.00038
     assert std == 0.0
-    print("{:02d} {:s} {:8.5f} {:6.4f}".format(sat, sig, bias, std))
+    print("{:s} {:s} {:8.5f} {:6.4f}".format(sat2id(sat), sig, bias, std))
